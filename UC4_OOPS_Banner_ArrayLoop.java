341cbf89f31feb7740c4851d0a23badeb4d45e8d
ujjwal3974-ctrl <ujjwal3974@gmail.com>
2026-02-23 11:29:12 +0530

    Updated the code

--- UC4_OOPS_Banner_ArrayLoop.java
+++ UC4_OOPS_Banner_ArrayLoop.java
@@ -1,22 +1,19 @@
 public class UC4_OOPS_Banner_ArrayLoop {
+
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-                String.join("", " *****", "   ", "*****", "   ", "*****", "    ", "*****"),
 
-                String.join("", " *   *", "   ", "*   *", "   ", "*   *", "   ", "*"),
+        String[] lines = new String[7];
 
-                String.join("", " *   *", "   ", "*   *", "   ", "*   *", "    ", "****"),
+        lines[0] = String.join(" ", "***", "***", "***", "***");
+        lines[1] = String.join(" ", "*  *", "*  *", "*", "*");
+        lines[2] = String.join(" ", "*  *", "*  *", "*", "*");
+        lines[3] = String.join(" ", "*  *", "***", "***", "***");
+        lines[4] = String.join(" ", "*  *", "*", "*", "*");
+        lines[5] = String.join(" ", "*  *", "*", "*", "*");
+        lines[6] = String.join(" ", "***", "*", "***", "***");
 
-                String.join("", " *   *", "   ", "*****", "   ", "*****", "        ", "*"),
-
-                String.join("", " *   *", "   ", "*    ", "   ", "*    ", "        ", "*"),
-
-                String.join("", " *   *", "   ", "*    ", "   ", "*    ", "   ", "*"),
-
-                String.join("", " *****", "   ", "*    ", "   ", "*    ", "   ", "*****")
-        };
-        for (String line : bannerLines) {
+        for (String line : lines) {
             System.out.println(line);
         }
     }
